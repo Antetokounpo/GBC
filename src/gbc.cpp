03d051ae6717fe303890cd269f174b5ecb5dbfc1
Antetokounpo <antor.232@outlook.com>
2019-12-14 13:11:24 -0500

    Add load and step functions

---
+++ src/gbc.cpp
@@ -0,0 +1,34 @@
+#include "gbc.hpp"
+
+bool GBC::load(const char* filename)
+{
+    if(!Z80::load(filename)) // function from parent class
+        return false;
+
+    std::memmove(memory + 0, rom + 0, 0x3FFF); /* 16KB ROM bank 00 */
+    std::memmove(memory + 0x4000, rom + 0x4000, 0x3FFF); /* 16KB ROM bank 01~NN */
+
+    return true;
+}
+
+void GBC::step()
+{
+    uint8_t opcode;
+    opcode = memory[pc];
+    execute(opcode);
+}
+
+void GBC::execute(uint8_t opcode)
+{
+    switch(opcode)
+    {
+        case 0x08:
+            ld(memory[get_operand(2)], sp);
+            pc += 3; break;
+        case 0x10:
+            // TODO STOP
+            break;
+        case 0x22: /* ldi (HL), A */
+            ld(HL.p, *A);
+    }
+}
--- src/gbc.hpp
+++ src/gbc.hpp
@@ -1,3 +1,10 @@
 #include "Z80/z80.hpp"
 
-class GBC : public Z80::Z80{};
+class GBC : public Z80::Z80
+{
+    public:
+        bool load(const char* filename);
+        void step();
+    protected:
+        void execute(uint8_t opcode);
+};
